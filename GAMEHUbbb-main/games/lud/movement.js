ea408bfa01113b62771c670602b188b296ca1d27
stevemoneys <adelekeopeyemi1001@gmail.com>
2026-04-17 19:40:42 +0100

    lll

--- GAMEHUbbb-main/games/lud/movement.js
+++
@@ -1,8 +0,0 @@
-export async function moveToken(tokenEl, path, startIndex, steps) {
-  for (let i = 1; i <= steps; i++) {
-    const nextIndex = (startIndex + i) % path.length;
-    const cellEl = path[nextIndex].el;
-    cellEl.appendChild(tokenEl);
-    await new Promise(r => setTimeout(r, 180));
-  }
-}
