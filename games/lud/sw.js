7cbb465cf9f54a6012038ce3a784e45ab7133192
stevemoneys <stevemoneys067@gmail.com>
2026-08-20 08:05:17 -0700

    k

--- games/lud/sw.js
+++ games/lud/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "2026-08-20-v4";
+const CACHE_VERSION = "2026-08-20-v5";
 const STATIC_CACHE = `ludo-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `ludo-runtime-${CACHE_VERSION}`;
 
