6e2a3c2c9a2cec03afda97b847cbe7b0e5233cff
stevemoneys <stevemoneys067@gmail.com>
2026-09-11 12:55:37 -0700

    g

--- games/lud/sw.js
+++ games/lud/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "2026-09-11-v79";
+const CACHE_VERSION = "2026-09-11-v81";
 const STATIC_CACHE = `ludo-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `ludo-runtime-${CACHE_VERSION}`;
 const PRELOAD_CACHE = "ludo-preload-assets-v1";
